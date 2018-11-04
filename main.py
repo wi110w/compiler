4860f56468ad4c92c04b8b7a98621d2dc7397e73
Eugenia Glinska <willow@nyan.org.ua>
2018-11-04 23:03:38 +0200

    Save main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from lexer import Lexer
+from tables import identifiers
 
-string = 'PROGRAM HelloWorld;'
+string = '(* Let it be some comment *)\n' \
+         'PROGRAM prog:( tell, well, sell );\n' \
+         '(* Let * be * some * stars * *)'
 lexer = Lexer()
 lexer.parse_string(string)
+print("Identifiers: ", identifiers)
