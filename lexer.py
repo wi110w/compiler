3dbb5bac1bf7302bddbcbd0b69c51fc7ab89032b
Eugenia Glinska <willow@nyan.org.ua>
2018-11-04 22:59:31 +0200

    Refactor and update Lexer
    - Add '(' handler
    - Add comments handler
    - Update errors handler
    - Refactor format of printing
    - Fix bugs with incorrect printing columns

--- lexer.py
+++ lexer.py
@@ -18,16 +18,20 @@ class Lexer:
         self.current_state = READY_STATE
         self.identifier = ''
         self.text = ''
+        self.symbol_counter = 0
         self.column = 0
         self.line = 0
 
     def parse_string(self, string):
-        self.column = 0
-        self.line = 0
+        self.column = 1
+        self.symbol_counter = 0
+        self.line = 1
         self.text = string
+        previous_lexeme = ''
         lexeme = ''
         begin_lexeme = 0
         while True:
+            # READY STATE
             if self.current_state == READY_STATE:
                 symbol = self.peek()
                 if not symbol:
@@ -38,69 +42,150 @@ class Lexer:
                     begin_lexeme = self.column
                     symbol = self.pop()
                     lexeme += symbol
-                elif 'A' <= symbol <= 'Z' or 'a' <= symbol <= 'z':
+                    continue
+                if 'A' <= symbol <= 'Z' or 'a' <= symbol <= 'z':
                     self.current_state = IDENTIFIER_STATE
                     begin_lexeme = self.column
                     symbol = self.pop()
                     lexeme += symbol
-                elif symbol in whitespaces:
-                    symbol = self.pop()
-                else:
-                    self.current_state = ERROR_STATE
+                    continue
+                if symbol == '(':
+                    self.current_state = BEGIN_COMMENT_STATE
                     begin_lexeme = self.column
-                    lexeme += symbol
                     symbol = self.pop()
+                    lexeme += symbol
+                    continue
+                if symbol in whitespaces:
+                    if symbol == '\n':
+                        self.line += 1
+                        self.column = 1
+                    symbol = self.pop()
+                    continue
+                self.current_state = ERROR_STATE
+                begin_lexeme = self.column
+                lexeme += symbol
+                symbol = self.pop()
                 continue
+
+            # IDENTIFIER STATE
             if self.current_state == IDENTIFIER_STATE:
                 symbol = self.peek()
                 if not symbol:
                     if lexeme in keywords:
-                        print("KEYWORD: " + lexeme + "\n Line: "
-                              + str(self.line) + ', column: ' + str(begin_lexeme))
+                        print("KEYWORD: {0}\n line: {1}, column: {2}".format(
+                            lexeme, str(self.line), str(begin_lexeme)))
                     else:
-                        print("IDENTIFIER: " + lexeme + "\n Line: "
-                              + str(self.line) + ', column: ' + str(begin_lexeme))
+                        print("IDENTIFIER: {0}\n line: {1}, column: {2}".format(
+                            lexeme, str(self.line), str(begin_lexeme)))
                         identifiers.add(lexeme)
 
                     self.current_state = READY_STATE
                     lexeme = ''
+                    continue
 
-                elif 'A' <= symbol <= 'Z' or 'a' <= symbol <= 'z' or '0' <= symbol <= '9':
+                if 'A' <= symbol <= 'Z' or 'a' <= symbol <= 'z' or '0' <= symbol <= '9':
                     symbol = self.pop()
                     lexeme += symbol
+                    continue
+                if lexeme in keywords:
+                    print("KEYWORD: {0}\n line: {1}, column: {2}".format(
+                        lexeme, str(self.line), str(begin_lexeme)))
                 else:
-                    if lexeme in keywords:
-                        print("KEYWORD: " + lexeme + "\n Line: "
-                              + str(self.line) + ', column: ' + str(begin_lexeme))
-                    else:
-                        print("IDENTIFIER: " + lexeme + "\n Line: "
-                              + str(self.line) + ', column: ' + str(begin_lexeme))
-                        identifiers.add(lexeme)
+                    print("IDENTIFIER: {0}\n line: {1}, column: {2}".format(
+                        lexeme, str(self.line), str(begin_lexeme)))
+                    identifiers.add(lexeme)
 
-                    self.current_state = READY_STATE
-                    lexeme = ''
-                continue
-            if self.current_state == DELIMITER_STATE:
-                print("DELIMITER: " + lexeme + "\n Line: "
-                      + str(self.line) + ', column: ' + str(begin_lexeme))
                 self.current_state = READY_STATE
                 lexeme = ''
                 continue
+
+            # DELIMITER STATE
+            if self.current_state == DELIMITER_STATE:
+                print("DELIMITER: {0}\n line: {1}, column: {2} ".format(
+                    lexeme, str(self.line), str(begin_lexeme)))
+                self.current_state = READY_STATE
+                lexeme = ''
+                continue
+
+            # SECOND DELIMITER STATE
+            if self.current_state == SECOND_DELIMITER_STATE:
+                print("DELIMITER: {0}\n line: {1}, column: {2}".format(
+                    lexeme, str(self.line), str(begin_lexeme)))
+                self.current_state = READY_STATE
+                lexeme = ''
+                continue
+
+            # ERROR STATE
             if self.current_state == ERROR_STATE:
                 self.current_state = READY_STATE
+                if previous_lexeme == '*':
+                    print("Error: Unexpected EOF inside the comment, line : {0}, column: {1}".format(
+                        self.line, begin_lexeme
+                    ))
+                    previous_lexeme = ''
+                    continue
                 print("Error: Unmatched input ('{0}'), line: {1}, column: {2}".format(
                              lexeme, self.line, begin_lexeme))
                 lexeme = ''
                 continue
 
+            # BEGIN COMMENT STATE
+            if self.current_state == BEGIN_COMMENT_STATE:
+                symbol = self.peek()
+                if symbol != '*':
+                    self.current_state = SECOND_DELIMITER_STATE
+                    continue
+                self.current_state = COMMENT_STATE
+                symbol = self.pop()
+                previous_lexeme += symbol
+                lexeme = ''
+                continue
+
+            # COMMENT STATE
+            if self.current_state == COMMENT_STATE:
+                symbol = self.peek()
+                if symbol == '*':
+                    self.current_state = END_COMMENT_STATE
+                    symbol = self.pop()
+                    continue
+                if not symbol:
+                    self.current_state = ERROR_STATE
+                    begin_lexeme = self.column
+                    continue
+                symbol = self.pop()
+                continue
+
+            # END COMMENT STATE
+            if self.current_state == END_COMMENT_STATE:
+                symbol = self.peek()
+                if symbol == ')':
+                    self.current_state = READY_STATE
+                    symbol = self.pop()
+                    continue
+                if symbol != '*':
+                    self.current_state = COMMENT_STATE
+                    symbol = self.pop()
+                    continue
+                if not symbol:
+                    self.current_state = ERROR_STATE
+                    begin_lexeme = self.column
+                    continue
+                symbol = self.pop()
+                continue
+
+
+
+
     def peek(self):
-        if self.column >= len(self.text):
+        if self.symbol_counter >= len(self.text):
             return None
-        return self.text[self.column]
+        return self.text[self.symbol_counter]
 
     def pop(self):
-        if self.column >= len(self.text):
+        if self.symbol_counter >= len(self.text):
             return None
-        symbol = self.text[self.column]
-        self.column += 1
+        symbol = self.text[self.symbol_counter]
+        self.symbol_counter += 1
+        if symbol != '\n':
+            self.column += 1
         return symbol
