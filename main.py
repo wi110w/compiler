4145b2eeb1bf872148079c2f75b0b8d91ed733b2
Eugenia Glinska <willow@nyan.org.ua>
2018-12-16 21:04:03 +0200

    Delete commented

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 from signal_lexer.lexer import Lexer
-from signal_lexer.tables import identifiers, constants
 from signal_parser.parser import Parser
 
 lexer_log = []
 lexer = Lexer()
-file = open('signal_lexer/tests.txt', 'r')
+file = open('parser-tests/program-procs.txt', 'r')
 samples = file.read()
 lexemes = lexer.parse_string(samples, lexer_log.append)
 file.close()
-# for log in lexer_log:
-#     print(log)
-# print("Identifiers: {0}".format(identifiers.keys()))
 
 parser = Parser(lexemes)
 parser.signal_program()
 parser.print_tree()
-# file = open('like-program.txt', 'r')
-# samples = file.read()
-# lexemes = signal_lexer.parse_string(samples)
-
-# print("Numeric constants: {0}".format(constants.keys()))
-# identifiers.clear()
-# constants.clear()
-# file.close()
